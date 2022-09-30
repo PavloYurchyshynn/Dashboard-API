66cebd4e300eff5d289794c7f69315bd56e0f34f
PavloYurchyshynn <gamepaha123@gmail.com>
2022-09-30 11:45:57 +0300

    feat: InversityJS

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,24 @@ import { ILogger } from './logger/logger.interface';
 import { ExeptionFilter } from './errors/exeption.filter';
 import { UserController } from './users/users.controller';
 import express, { Express } from 'express';
-import { Server } from 'http'
+import { Server } from 'http';
+import { injectable, inject } from 'inversify'
+import { TYPES } from './types';
+import 'reflect-metadata'
 
+@injectable()
 export class App {
     app: Express
     server: Server
     port: number
-    logger: ILogger
-    userController: UserController
-    exeptionFilter: ExeptionFilter
 
     constructor(
-        logger: ILogger,
-        userController: UserController,
-        exeptionFilter: ExeptionFilter
+        @inject(TYPES.ILogger) private logger: ILogger,
+        @inject(TYPES.UserController) private userController: UserController,
+        @inject(TYPES.ExeptionFilter) private exeptionFilter: ExeptionFilter,
     ) {
         this.app = express()
         this.port = 8000
-        this.logger = logger
-        this.userController = userController
-        this.exeptionFilter = exeptionFilter
     }
 
     useRouter() {
