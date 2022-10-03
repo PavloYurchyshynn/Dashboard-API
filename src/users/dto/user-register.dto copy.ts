b9d52875c68b0144ebfa9f7b732ea356b998dbac
PavloYurchyshynn <gamepaha123@gmail.com>
2022-10-03 13:51:50 +0300

    feat: add data validation

--- src/users/dto/user-register.dto copy.ts
+++ src/users/dto/user-register.dto copy.ts
@@ -1,5 +1,12 @@
+import { IsEmail, IsString } from 'class-validator';
+
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'Не правильно вказаний email' })
 	email: string;
+
+	@IsString({ message: 'Не вказаний пароль' })
 	password: string;
+
+	@IsString({ message: 'Не вказано імя' })
 	name: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,7 @@ import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto copy';
 import { User } from './user.entity';
 import { UserService } from './user.service';
+import { ValidateMiddleware } from '../common/validate.middleware';
 
 @injectable()
 export class UserController extends BaseController implements IUsersController {
@@ -19,7 +20,12 @@ export class UserController extends BaseController implements IUsersController {
 	) {
 		super(loggerService);
 		this.bindRoutes([
-			{ path: '/register', method: 'post', func: this.register },
+			{
+				path: '/register',
+				method: 'post',
+				func: this.register,
+				middleWares: [new ValidateMiddleware(UserRegisterDto)],
+			},
 			{ path: '/login', method: 'post', func: this.login },
 		]);
 	}
